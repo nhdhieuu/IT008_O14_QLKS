f57610ffadad82023a13292506e0012286b55f71
huyhoang462 <22520462@gm.uit.edu.vn>
2023-10-24 09:35:36 +0700

    add staff p1

---
+++ IT008_O14_QLKS/View/Manager/staff.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IT008_O14_QLKS.View.Manager
+{
+    /// <summary>
+    /// Interaction logic for staff.xaml
+    /// </summary>
+    public partial class staff : UserControl
+    {
+        public staff()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            datelb.Content = DateTime.Now.ToString("MMM, dd, yyyy");
+        }
+    }
+}

47258ed9490ee40491fc2bc95270cbff0562f38b
nhdhieuu <22520439@gm.uit.edu.vn>
2023-10-26 22:23:38 +0700

    modify gitignore

--- IT008_O14_QLKS/obj/Debug/IT008_O14_QLKS_Content.g.i.cs
+++ IT008_O14_QLKS/obj/Debug/IT008_O14_QLKS_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/calendar.png")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/printer.png")]
 
 

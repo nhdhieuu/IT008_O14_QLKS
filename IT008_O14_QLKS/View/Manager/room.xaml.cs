78af28bccb0ce555a5b449728115eaf47ccc508c
KKuyen <144935744+KKuyen@users.noreply.github.com>
2023-10-26 17:50:18 +0700

    merge branch cardroomtest

--- IT008_O14_QLKS/View/Manager/room.xaml.cs
+++ IT008_O14_QLKS/View/Manager/room.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IT008_O14_QLKS.View.Manager.Card;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,21 +24,94 @@ namespace IT008_O14_QLKS.View.Manager
         public room()
         {
             InitializeComponent();
-        }
+            roomcard[] listCard = new roomcard[6];
+            // -- Load các giá trị cho 6 ô từ data base
+            listCard[0]= new roomcard("P101", "VIP", "Trần Văn A", "day", 2, 2);
+            listCard[1] = new roomcard("P101", "VIP", "Trần Văn A", "day", 2, 2);
+            listCard[2] = new roomcard("P101", "Normal", "Trần Văn A", "hour", 2, 2);
+            listCard[3] = new roomcard("P101", "VIP", "Trần Văn A", "day", 2, 2);
+            listCard[4] = new roomcard("P101", "VIP", "Trần Văn A", "day", 2, 2);
+            listCard[5] = new roomcard("P101", "VIP", "Trần Văn A", "day", 2, 2);
+          
 
+            //
+
+             
+          
+            cc11.Content = listCard[0].Content;
+            cc21.Content = listCard[1].Content;
+            cc12.Content = listCard[2].Content;
+            cc22.Content = listCard[3].Content;
+            cc13.Content = listCard[4].Content;
+            cc23.Content = listCard[5].Content;
+           
+
+
+        }
+        int cleaned = 1;
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
+          if(cleaned == 1) {
+                Clean.Background = new SolidColorBrush(Colors.Green);
+                cleaned = 2;
+            }
+            else if (cleaned == 2)
+            {
+                Clean.Background = new SolidColorBrush(Colors.Red);
+                cleaned = 3;
+            }
+           else if (cleaned == 3)
+            {
+                Clean.Background = new SolidColorBrush(Colors.Gray);
+                cleaned = 1;
+            }
+
+
+
 
         }
 
         private void Border_MouseEnter(object sender, MouseEventArgs e)
         {
-            Clean.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF0E9755"));
+            if (cleaned == 1)
+            {
+                Clean.Background = new SolidColorBrush(Colors.DarkGray);
+               
+            }
+            else if (cleaned == 2)
+            {
+                Clean.Background = new SolidColorBrush(Colors.DarkGreen);
+               
+            }
+            else if (cleaned == 3)
+            {
+                Clean.Background = new SolidColorBrush(Colors.DarkRed);
+              
+            }
+
+
+
         }
 
         private void Border_MouseLeave(object sender, MouseEventArgs e)
         {
-            Clean.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF66BB6A"));
+            if (cleaned == 1)
+            {
+                Clean.Background = new SolidColorBrush(Colors.Gray);
+               
+            }
+            else if (cleaned == 2)
+            {
+                Clean.Background = new SolidColorBrush(Colors.Green);
+               
+            }
+            else if (cleaned == 3)
+            {
+                Clean.Background = new SolidColorBrush(Colors.Red);
+               
+            }
+
+
         }
 
         private void Border_MouseDown_1(object sender, MouseButtonEventArgs e)
@@ -54,5 +128,17 @@ namespace IT008_O14_QLKS.View.Manager
         {
             add.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF66BB6A"));
         }
+
+        private void Border_MouseEnter_2(object sender, MouseEventArgs e)
+        {
+            calender.Background= new SolidColorBrush(Colors.Black);
+        }
+
+        private void calender_MouseLeave(object sender, MouseEventArgs e)
+        {
+            calender.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF605C5C"));
+
+        }
     }
+    
 }

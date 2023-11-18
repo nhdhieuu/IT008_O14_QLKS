aad10248784d96ea771144324c24a09a1b97345f
NDHunq <22520514@gm.uit.edu.vn>
2023-11-19 00:01:32 +0700

    Hoan thanh giao dien ViewRoomForm

--- IT008_O14_QLKS/View/Manager/FormPage/room/Viewroom_form.xaml.cs
+++ IT008_O14_QLKS/View/Manager/FormPage/room/Viewroom_form.xaml.cs
@@ -23,5 +23,60 @@ namespace IT008_O14_QLKS.View.Manager.FormPage.room
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Border_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            ViewRoomWD.Close();
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            CloseBD.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#740909"));
+            CLoseTXT.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            CloseBD.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF1F0E7"));
+            CLoseTXT.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#740909"));
+        }
+
+        private void ViewRoomWD_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void add_butt_MouseEnter(object sender, MouseEventArgs e)
+        {
+            add_butt.Background= new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF057832"));
+        }
+
+        private void add_butt_MouseLeave(object sender, MouseEventArgs e)
+        {
+            add_butt.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF27CF69"));
+        }
+
+        private void add_buttt_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void add_buttt_MouseEnter(object sender, MouseEventArgs e)
+        {
+            add_buttt.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF057832"));
+
+        }
+
+        private void add_buttt_MouseLeave(object sender, MouseEventArgs e)
+        {
+            add_buttt.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF27CF69"));
+
+        }
     }
 }

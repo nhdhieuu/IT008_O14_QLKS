413b0061bc86b055d8d8705add3f223530ed9235
nhdhieuu <22520439@gm.uit.edu.vn>
2023-11-18 15:47:01 +0700

    resolve receipt page and add ClientControl

---
+++ IT008_O14_QLKS/View/Manager/ClientControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace IT008_O14_QLKS.View.Manager
+{
+    public partial class ClientControl : UserControl
+    {
+        public ClientControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

b45f01a82a25407761c6a7d098be72697b933951
nhdhieuu <22520439@gm.uit.edu.vn>
2023-12-10 21:10:58 +0700

    adding Clients folder

---
+++ IT008_O14_QLKS/View/Clients/ClientsMain.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace IT008_O14_QLKS.View.Clients
+{
+    public partial class ClientsMain : Window
+    {
+        public ClientsMain()
+        {
+            InitializeComponent();
+        }
+    }
+}

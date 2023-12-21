323f229d8196fcbb2d499295e9a8402f00df4ce6
nhdhieuu <22520439@gm.uit.edu.vn>
2023-12-21 17:14:55 +0700

    display content and search

--- IT008_O14_QLKS/View/Manager/receipt.xaml.cs
+++ IT008_O14_QLKS/View/Manager/receipt.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IT008_O14_QLKS.Connection_db;
 using IT008_O14_QLKS.View.Manager.Card;
 
 namespace IT008_O14_QLKS.View.Manager
@@ -21,28 +23,77 @@ namespace IT008_O14_QLKS.View.Manager
     /// </summary>
     public partial class receipt : UserControl
     {
+        DB_connection _db = new DB_connection();
+        
+        
         public receipt()
         {
             InitializeComponent();
-            ReceiptCard[] listCard = new ReceiptCard[4];
+            this.Loaded += receipt_Loaded;
 
-            listCard[0] = new ReceiptCard("1", "12/12/2012", "12:12", "100000");
-            listCard[1] = new ReceiptCard("2", "12/12/2012", "12:12", "100000");
-            listCard[2] = new ReceiptCard("3", "12/12/2012", "12:12", "100000");
-            listCard[3] = new ReceiptCard("4", "12/12/2012", "12:12", "100000");
-
-             
-          
-            Cc11.Content = listCard[0].Content;
-            cc12.Content = listCard[1].Content;
-            cc13.Content = listCard[2].Content;
-            cc14.Content = listCard[3].Content;
-            cc15.Content = listCard[3].Content;
-            cc16.Content = listCard[3].Content;
-            cc17.Content = listCard[3].Content;
-            cc18.Content = listCard[3].Content;
-            cc19.Content = listCard[3].Content;
             
+            
+        }
+
+        private void receipt_Loaded(object sender, RoutedEventArgs e)
+        {
+            SqlCommand sqlCommand= new SqlCommand();
+            sqlCommand.Connection = _db.sqlCon;
+            sqlCommand.CommandType = System.Data.CommandType.Text;
+            sqlCommand.CommandText = "select sohd,NgayLap,CAST(ngaylap AS time),tongtien from hoadon";
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            while (sqlDataReader.Read())
+            {
+                
+                ReceiptCard receiptCard =new ReceiptCard(sqlDataReader[0].ToString(),sqlDataReader[1],sqlDataReader[2].ToString(),sqlDataReader[3].ToString());
+                ContentControl contentControl = new ContentControl();
+                if(ReceiptCardPanel.Children.Count >= 1) {
+                    // Child thứ 2, set margin 
+                    contentControl.Margin = new Thickness(0, 10, 0, 0);  
+                }
+                
+                contentControl.Content = receiptCard.Content;
+                ReceiptCardPanel.Children.Add(contentControl);
+            }
+            sqlDataReader.Close();
+        }
+
+        private void Search_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (SearchBox.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập số hóa đơn cần tìm kiếm");
+            }
+            else
+            {
+                ReceiptCardPanel.Children.Clear();;
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = _db.sqlCon;
+                sqlCommand.CommandType = System.Data.CommandType.Text;
+                sqlCommand.CommandText = "select sohd,NgayLap,CAST(ngaylap AS time),tongtien from hoadon where sohd = '" + SearchBox.Text + "'";
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                if (sqlDataReader.HasRows)
+                {
+                    while (sqlDataReader.Read())
+                    {
+                        ReceiptCard receiptCard = new ReceiptCard(sqlDataReader[0].ToString(), sqlDataReader[1], sqlDataReader[2].ToString(), sqlDataReader[3].ToString());
+                        ContentControl contentControl = new ContentControl();
+                        if (ReceiptCardPanel.Children.Count >= 1)
+                        {
+                            // Child thứ 2, set margin 
+                            contentControl.Margin = new Thickness(0, 10, 0, 0);
+                        }
+
+                        contentControl.Content = receiptCard.Content;
+                        ReceiptCardPanel.Children.Add(contentControl);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy hóa đơn");
+                }
+                sqlDataReader.Close();
+            }
         }
     }
 }
